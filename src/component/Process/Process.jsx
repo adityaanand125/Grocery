45ba73362dfe238b9daa2de8f3a1c88af0196cf1
Aditya Anand <adityaanand1365@gmail.com>
2026-02-02 23:38:26 +0530

    Updated Navbar and Process components

--- src/component/Process/Process.jsx
+++ src/component/Process/Process.jsx
@@ -1,82 +1,93 @@
 import React from 'react'
 import Heading from '../Heading/Heading'
-import { TbCircleNumber1Filled, TbCircleNumber2Filled, TbCircleNumber3Filled, TbCircleNumber4Filled } from "react-icons/tb";
-import { PiFactory,PiPlant} from "react-icons/pi";
-import { BsTruck } from "react-icons/bs";
-import { SlBadge } from "react-icons/sl";
+import {
+  TbCircleNumber1Filled,
+  TbCircleNumber2Filled,
+  TbCircleNumber3Filled,
+  TbCircleNumber4Filled
+} from "react-icons/tb"
+import { PiFactory, PiPlant } from "react-icons/pi"
+import { BsTruck } from "react-icons/bs"
+import { SlBadge } from "react-icons/sl"
 
 const Process = () => {
 
-    const rendetSteps = steps.map(item => {
-        return (
-            <div className={`flex-1 basis-[300px] ${item.id % 2 === 0 ? 'md:-mt-90' : ''}`}>
-                <span className='flex justify-center items-center w-15 h-15  mx-auto rounded-full text-6xl bg-zinc-800 text-white outline-[3px] outline-offset-7 outline-zinc-800 outline-dashed '>
-                    {item.number}
-                </span>
-                <div className='flex items-center mt-10 '>
-                    
-                        <span className='flex justify-center items-center text-3xl bg-gradient-to-b from-orange-400 to-orange-500 text-white h-15 w-15 rounded-full mr-4'>
-                            {item.icon}
-                        </span>
-                    
-                    <div className='flex-1'>
-                        <h4 className='text-zinc-800 text-2xl font-bold'>
-                            {item.title}
-                        </h4>
-                        <p className='text-zinc-600 mt-2'>
-                            {item.para}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        )
-    })
+  const renderSteps = steps.map((item) => {
     return (
-        <section>
+      <div
+        key={item.id}
+        className={`flex-1 basis-[300px] ${item.id % 2 === 0 ? 'md:-mt-90' : ''}`}
+      >
+        {/* Step Number */}
+        <span className='flex justify-center items-center w-15 h-15 mx-auto rounded-full text-6xl bg-zinc-800 text-white outline-[3px] outline-offset-7 outline-zinc-800 outline-dashed'>
+          {item.number}
+        </span>
 
-            <div className='max-w-[1200px] mx-auto px-10 py-20'>
-                <div className='w-fit mr-auto'>
-                    <Heading highlight="Our" Heading=" Process" />
-                </div>
-                <div className='flex max-md:flex-wrap gap-y-17 items-center justify-center  md:mt-20 mt-10 md:pt-50'>
-                    {rendetSteps}
-                </div>
+        {/* Content */}
+        <div className='flex items-center mt-10'>
+          <span className='flex justify-center items-center text-3xl bg-gradient-to-b from-orange-400 to-orange-500 text-white h-15 w-15 rounded-full mr-4'>
+            {item.icon}
+          </span>
 
-            </div>
-        </section>
+          <div className='flex-1'>
+            <h4 className='text-zinc-800 text-2xl font-bold'>
+              {item.title}
+            </h4>
+            <p className='text-zinc-600 mt-2'>
+              {item.para}
+            </p>
+          </div>
+        </div>
+      </div>
     )
+  })
+
+  return (
+    <section>
+      <div className='max-w-[1200px] mx-auto px-10 py-20'>
+        <div className='w-fit mr-auto'>
+          <Heading highlight="Our" Heading=" Process" />
+        </div>
+
+        <div className='flex max-md:flex-wrap gap-y-17 items-center justify-center md:mt-20 mt-10 md:pt-50'>
+          {renderSteps}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Process
 
-const steps = [
-    {
-        id: 1,
-        number: <TbCircleNumber1Filled />,
-        title: 'Sourcing',
-        para: 'It is a long established fact that a reader',
-        icon: <PiPlant />,
-    },
-    {
-        id: 2,
-        number: <TbCircleNumber2Filled />,
-        title: 'Manufacturing',
-        para: 'It is long established fact that a reader',
-        icon: <PiFactory />,
-    },
-    {
-        id: 3,
-        number: <TbCircleNumber3Filled />,
-        title: 'Quality Control',
-        para: 'It is long established fact that a reader ',
-        icon: <SlBadge />,
-    },
+// ===================== DATA =====================
 
-    {
-        id: 4,
-        number: <TbCircleNumber4Filled />,
-        title: 'Logistics',
-        para: 'It is a long established fact that a reader',
-        icon: <BsTruck />,
-    }
+const steps = [
+  {
+    id: 1,
+    number: <TbCircleNumber1Filled />,
+    title: 'Sourcing',
+    para: 'It is a long established fact that a reader',
+    icon: <PiPlant />,
+  },
+  {
+    id: 2,
+    number: <TbCircleNumber2Filled />,
+    title: 'Manufacturing',
+    para: 'It is long established fact that a reader',
+    icon: <PiFactory />,
+  },
+  {
+    id: 3,
+    number: <TbCircleNumber3Filled />,
+    title: 'Quality Control',
+    para: 'It is long established fact that a reader',
+    icon: <SlBadge />,
+  },
+  {
+    id: 4,
+    number: <TbCircleNumber4Filled />,
+    title: 'Logistics',
+    para: 'It is a long established fact that a reader',
+    icon: <BsTruck />,
+  },
 ]
