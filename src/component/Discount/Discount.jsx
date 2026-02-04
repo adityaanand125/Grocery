43f9f634b5fa785895ef39e0d3f2db6b3aa820a0
Aditya Anand <adityaanand1365@gmail.com>
2026-02-04 23:20:46 +0530

    Updated discount

--- src/component/Discount/Discount.jsx
+++ src/component/Discount/Discount.jsx
@@ -5,7 +5,7 @@ const Discount = () => {
     return (
         <section className='bg-zinc-100 bg-no-repeat bg-right bg-contain' style={{backgroundImage: `url(${BgDis})`}}>
             <div className=' md:flex-row flex-col max-w-[1200px] mx-auto px-10 flex py-10'>
-                <span className='text-5xl md:text-9xl text-orange-500 font-bold transform md:-rotate-90 h-fit md:self-center'>20%</span>
+                <span className='text-5xl md:text-9xl text-orange-500 font-bold transform md:-rotate-90 h-fit md:self-center'>30%</span>
                 <div className='max-w-[600px] '>
                     <h3 className='text-4xl md:text-7xl text-zinc-800 font-bold'>Frist order Discount!</h3>
                     <p className=' text-zinc-600 my-6'>
